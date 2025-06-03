49dc9521b38569b007811acde4fa2758f67b915f
Kirill-dev-dev <coolparty4.20pm@gmail.com>
2025-06-03 13:33:37 +0300

    Update: общие Улучшения

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,15 +17,9 @@ export const metadata: Metadata = {
   title: "Smart Living",
   description: "Smart Living - The best place to buy homeware",
   icons: {
-    icon: [
-      { url: '/site-ico.ico', sizes: 'any' },
-      { url: '/site-ico.ico', type: 'image/png', sizes: '32x32' },
-      { url: '/site-ico.ico', type: 'image/png', sizes: '180x180' },
-    ],
-    apple: [
-      { url: '/site-ico.ico', sizes: '180x180' },
-    ],
-    shortcut: ['/site-ico.ico'],
+    icon: '/site-ico.ico',
+    apple: '/site-ico.ico',
+    shortcut: '/site-ico.ico',
   },
   appleWebApp: {
     capable: true,
@@ -42,6 +36,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
+      <head>
+        <link rel="icon" href="/site-ico.ico" />
+        <link rel="apple-touch-icon" href="/site-ico.ico" />
+        <link rel="shortcut icon" href="/site-ico.ico" />
+      </head>
       <body className={inter.className}>
         <BasketProvider>
           <MobileLayout>
